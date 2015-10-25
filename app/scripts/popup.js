a58bf99d401f45c67ea4cc4bab6de93c0323cd2a
kommander <sebastian@formzoo.com>
2015-10-25 14:32:30 +0100

    Add icons

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -4,20 +4,24 @@ var query = { active: true, currentWindow: true };
 var trafficData = chrome.extension.getBackgroundPage().trafficData;
 var persistentData = chrome.extension.getBackgroundPage().persistentData;
 
+var noDataStr = 'no data';
+
 function callback(tabs) {
   var currentTab = tabs[0];
   var domain = currentTab.url.match(/^[\w-]+:\/*\[?([\w\.:-]+)\]?(?::\d+)?/)[1].replace('www.', '');
   document.getElementById('current_domain').innerHTML = domain;
-  document.getElementById('footprint_accumulated').innerHTML = chrome.extension.getBackgroundPage().bytesToCO2(persistentData.total[domain]) + 'g of CO2';
-  document.getElementById('page_size').innerHTML = Math.round(trafficData[currentTab.id] / 1024 / 1024 * 100) / 100 + ' MB';
-  var grams = chrome.extension.getBackgroundPage().bytesToCO2(trafficData[currentTab.id]);
-  document.getElementById('page_footprint').innerHTML = grams + 'g of CO2';
-  var liters = chrome.extension.getBackgroundPage().gramsToLiters(grams);
-  document.getElementById('footprint_volume').innerHTML = liters + ' Liters';
+  var accumulated = chrome.extension.getBackgroundPage().bytesToCO2(Math.max(persistentData.total[domain], 0));
+  document.getElementById('footprint_accumulated').innerHTML = accumulated ? accumulated + 'g of CO2' : noDataStr;
+  var traffic = Math.max(Math.round(trafficData[currentTab.id] / 1024 / 1024 * 100) / 100, 0);
+  document.getElementById('page_size').innerHTML = traffic ? traffic + ' MB' : noDataStr;
+  var grams = Math.max(chrome.extension.getBackgroundPage().bytesToCO2(trafficData[currentTab.id]), 0);
+  document.getElementById('page_footprint').innerHTML = grams ? grams + 'g of CO2' : noDataStr;
+  var liters = Math.max(chrome.extension.getBackgroundPage().gramsToLiters(grams), 0);
+  document.getElementById('footprint_volume').innerHTML = liters ? liters + ' Liters' : noDataStr;
   var mate = '<div class="bottle"></div>';
   var bottles = [];
   var sum = liters / 0.5;
-  for (var i = sum; i >= 0; i--) {
+  for (var i = sum; i >= 1; i--) {
     bottles.push(mate);
   }
   var str = bottles.join('');
