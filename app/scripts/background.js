8ac7ee279e791cf8375187ebaff9f69330cc6a83
kommander <sebastian@formzoo.com>
2015-10-25 13:50:41 +0100

    stuff

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -9,7 +9,17 @@ if (!persistentData.total) {
 	persistentData.total = {};
 }
 
+var co2ConversionRate = 7.22;
+var pushIntervalTime = 500;
 var bytesToPush = 0;
+
+function bytesToCO2(bytes) {
+	return Math.round(bytes / 1024 / 1024 * co2ConversionRate);
+}
+
+function gramsToLiters(grams) {
+	return Math.round(grams * (559 / 1000) * 1000) / 1000;
+}
 
 // CO2/Traffic Conversion: 1 MB / 0.722g CO2
 
@@ -53,14 +63,17 @@ chrome.windows.onRemoved.addListener(function () {
 	localStorage.setItem('ECNUD', JSON.stringify(persistentData));
 });
 
+var pushing = false;
 setInterval(function () {
-	console.log('checking bytes to push...');
-	if (bytesToPush > 0) {
-		console.log('pushing');
-		fetchival('http://10.211.55.13:3000/pushVolume?bytes=' + bytesToPush, { responseAs: 'text' }).get()['catch'](function (err) {
+	if (gramsToLiters(bytesToCO2(bytesToPush)) > 0 && !pushing) {
+		pushing = true;
+		var temp = bytesToPush;
+		bytesToPush = 0;
+		console.log('Pushing', temp, gramsToLiters(bytesToCO2(temp)));
+		fetchival('http://10.211.55.13:3000/pushVolume?bytes=' + temp, { responseAs: 'text' }).get()['catch'](function (err) {
 			console.log(err);
 		});
-		bytesToPush = 0;
+		pushing = false;
 	}
-}, 3000);
+}, pushIntervalTime);
 //# sourceMappingURL=background.js.map
--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -3,21 +3,16 @@
 var query = { active: true, currentWindow: true };
 var trafficData = chrome.extension.getBackgroundPage().trafficData;
 var persistentData = chrome.extension.getBackgroundPage().persistentData;
-var co2ConversionRate = 7.22;
-
-function bytesToCO2(bytes) {
-  return Math.round(bytes / 1024 / 1024 * co2ConversionRate);
-}
 
 function callback(tabs) {
   var currentTab = tabs[0];
   var domain = currentTab.url.match(/^[\w-]+:\/*\[?([\w\.:-]+)\]?(?::\d+)?/)[1].replace('www.', '');
   document.getElementById('current_domain').innerHTML = domain;
-  document.getElementById('footprint_accumulated').innerHTML = bytesToCO2(persistentData.total[domain]) + 'g of CO2';
+  document.getElementById('footprint_accumulated').innerHTML = chrome.extension.getBackgroundPage().bytesToCO2(persistentData.total[domain]) + 'g of CO2';
   document.getElementById('page_size').innerHTML = Math.round(trafficData[currentTab.id] / 1024 / 1024 * 100) / 100 + ' MB';
-  var grams = bytesToCO2(trafficData[currentTab.id]);
+  var grams = chrome.extension.getBackgroundPage().bytesToCO2(trafficData[currentTab.id]);
   document.getElementById('page_footprint').innerHTML = grams + 'g of CO2';
-  var liters = Math.round(grams * (559 / 1000) * 100) / 100;
+  var liters = chrome.extension.getBackgroundPage().gramsToLiters(grams);
   document.getElementById('footprint_volume').innerHTML = liters + ' Liters';
   var mate = '<div class="bottle"></div>';
   var bottles = [];
